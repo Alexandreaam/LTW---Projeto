a382d58c8bdf8407eddc1a38f18731bef89187a7
Alexandreaam <alex.al.mar@hotmail.com>
2017-12-06 00:23:24 +0000

    Add files via upload
    Fix falhas de segurança do html

--- FirstPage.php
+++ FirstPage.php
@@ -3,23 +3,23 @@
     <button onclick="log()" id="logbot">Login</button>
 </section>
 <nav id="blocos">
-    <ul id="registo">
-        <h1>Register</h1>
-        <form id="regform" action="Registo.php" method="POST">
-            <li><label>E-mail <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required="required"></label></li>
+    <form id="regform" action="Registo.php" method="POST">
+        <ul id="registo">
+            <li><label id="titleres">Register</label></li>
+                <li><label>E-mail <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required="required"></label></li>
+                <li><label>Username <input type="text" name="username" required="required"></label></li>
+                <li><label>Password <input type="password" name="password" required="required"></label></li>
+                <li><label>Date of birth <input type="date" name="date" required="required"></label></li>
+            <li><input type="submit"></li>
+        </ul>
+    </form>
+    <form id="logform" action="Login.php" method="POST">
+        <ul id="login">
+            <li><label id="titlelog">Login</label></li>
             <li><label>Username <input type="text" name="username" required="required"></label></li>
             <li><label>Password <input type="password" name="password" required="required"></label></li>
-            <li><label>Date of birth <input type="date" name="date" required="required"></label></li>
-            <input type="submit">
-        </form>
-    </ul>
-    <ul id="login">
-        <h1>Login</h1>
-        <form id="logform" action="Login.php" method="POST">
-            <li><label>Username <input type="text" name="username" required="required"></label></li>
-            <li><label>Password <input type="password" name="password" required="required"></label></li>
-            <input type="submit">
-        </form>
-    </ul>
+            <li><input type="submit"></li>
+        </ul>
+    </form>
 </nav>
 
